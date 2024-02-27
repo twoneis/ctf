92afc82564b3810b4df8bbb033f6b2e62536dbfd
twoneis <sanjay29@sapura.de>
2024-02-28 00:31:26 +0100

    added dirs to start pwn

---
+++ studsec-ctf/pwn/flgcmp/flgcmp.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <string.h>
+
+int main() {
+    FILE *fd;
+    char input[80];
+    char flag[80];
+
+    fd = fopen("./flag.txt", "r");
+
+    fgets(flag, 80, fd);
+
+    printf("Whats the password?\n");
+
+    if (fgets(input, 80, stdin) != NULL) {
+        if (strcmp(input, flag) != 0) {
+            printf("Incorrect flag, you have lost root privileges!\n");
+            setuid(getuid());   // Drop privileges
+        } else {
+            printf("Welcome root\n");
+        }
+
+        char *args[] = {"sh", 0};
+        execvp("/bin/sh", args);
+    }
+
+    return 0;
+}
