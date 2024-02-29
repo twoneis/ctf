749510288d05a399690479dc47036eb3afb997df
twoneis <sanjay29@sapura.de>
2024-02-29 16:01:29 +0100

    python 3.7 for flag-packer

--- flake.nix
+++ flake.nix
@@ -1,10 +1,11 @@
 {
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    nixpkgs-python.url = "github:cachix/nixpkgs-python";
     utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, ... }@inputs: inputs.utils.lib.eachSystem [
+  outputs = { self, nixpkgs, nixpkgs-python, ... }@inputs: inputs.utils.lib.eachSystem [
     "x86_64-linux" "i686-linux" "aarch64-linux" "x86_64-darwin"
   ] (system: let
       pkgs = import nixpkgs {
@@ -15,20 +16,21 @@
 
         targetPkgs = pkgs: with pkgs; [
           (cutter.withPlugins (pkgs: with pkgs; [cutterPlugins.rz-ghidra]))
-          ghidra
 
           pwndbg
+          ltrace
 
           (python3.withPackages (python-pkgs: with python311Packages; [
             python-lsp-server
             pwntools
           ]))
 
-          ltrace
-
           clang-tools
           libllvm
           clang
+
+          # for studsec-ctf/reversing/flag-packer
+          nixpkgs-python.packages.x86_64-linux."3.7"
         ];
 
         runScript = ''
